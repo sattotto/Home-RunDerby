9c1c74221853007509ec5c343c993dd9055dc3f1
佐藤秀輔 <sato.fun0619@gmail.com>
2017-11-17 00:45:50 +0900

    add new sample

---
+++ Doteater/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject target;
+	UnityEngine.AI.NavMeshAgent agent;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = target.transform.position;
+		animator.SetFloat("Speed", agent.velocity.magnitude);
+	}
+}
