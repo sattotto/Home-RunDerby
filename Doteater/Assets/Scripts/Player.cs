058503ce0328d6d5242e1fc4689bc538ef401dcd
KeitaroArata <b1015240@fun.ac.jp>
2017-11-17 10:43:19 +0900

    ぶち込んでやるぜ

--- Doteater/Assets/Scripts/Player.cs
+++ Doteater/Assets/Scripts/Player.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap;
+using System.Diagnostics;
 
 public class Player : MonoBehaviour {
 
 	public float moveSpead = 5f;
 	public float rotationSpeed = 360f;
+
+	public float RightMove;
+	public float LeftMove;
+	public float UpMove;
+	public float DownMove;
 	
 	CharacterController characterController;
 	Animator animator;
+	Controller controller = new Controller();
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +24,49 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {	
+	void Update () {
+
+	///	if(RightMove > 0.5)
+
+
+	Frame frame = controller.Frame();
+		RightMove = frame.Hands.Rightmost.GrabStrength;
+		LeftMove  = frame.Hands.Leftmost.GrabStrength;
+		UpMove    = frame.Hands.Rightmost.PinchStrength;
+		DownMove  = frame.Hands.Leftmost.PinchStrength;
+
+		if (RightMove > 0.5) {
+			Process.Start (
+				new ProcessStartInfo {
+					FileName = "osascript",
+					Arguments = "-e 'tell application \"System Events\" to key code 124'"
+				}
+			);
+		}else if(LeftMove > 0.5){
+			Process.Start (
+				new ProcessStartInfo {
+					FileName = "osascript",
+					Arguments = "-e 'tell application \"System Events\" to key code 123'"
+				}
+			);
+		}
+
+		if (UpMove > 0.5) {
+			Process.Start (
+				new ProcessStartInfo {
+					FileName = "osascript",
+					Arguments = "-e 'tell application \"System Events\" to key code 126'"
+				}
+			);
+		}else if(DownMove > 0.5){
+			Process.Start (
+				new ProcessStartInfo {
+					FileName = "osascript",
+					Arguments = "-e 'tell application \"System Events\" to key code 125'"
+				}
+			);
+		}
+		
 		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		if (direction.sqrMagnitude > 0.01f) {
 			Vector3 forward = Vector3.Slerp(
